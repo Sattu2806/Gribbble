9b8d8c60c119ad1d39c888ea5b05b46024b1f1a9
satender maurya <maurya.9@iitj.ac.in>
2024-10-15 05:50:37 +0530

    upload-new page 404 fix

---
+++ src/app/upload-new/page.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react";
+import ShowPage from "../(Show)/_components/Homepage";
+
+export default function Home() {
+
+  return (
+    <main>
+      <Suspense fallback={<div>loading...</div>}>
+        <ShowPage/>
+      </Suspense>
+    </main>
+  );
+}
